058cd72816d0a0c31753b23f80a7119b102c1bae
Agnes Chang <kradeki+datavis-cph2018@gmail.com>
2018-07-09 15:44:10 +0200

    big blue circle

--- sketch.js
+++ sketch.js
@@ -7,7 +7,8 @@ function draw() {
   //this circle will follow my mouse
   fill("gray");
   ellipse(mouseX,mouseY,40,40);
-
+  fill("blue");
+  ellipse(100,200,100,100);
   fill("orange");
   textSize(32); // big text
   text("happy copenhagen", 50, 100);
